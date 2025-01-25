4f87ff99e6f0c58ae8299da043be51cb3112728c
YUGAL BANSAL <BANSALYUGAL3@GMAIL.COM>
2025-01-25 14:37:03 +0530

    Solution to Check Equal To

---
+++ Check Equal To@@67882a731b4392d8fc1577da/code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // printf("%s", welcome());
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a==b)
+    printf("True");
+    else
+    printf("False");
+    return 0;
+}
