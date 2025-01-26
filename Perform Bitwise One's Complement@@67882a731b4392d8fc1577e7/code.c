d518070296059ce08cbf505f6423a5a0da368a78
YUGAL BANSAL <BANSALYUGAL3@GMAIL.COM>
2025-01-26 10:39:25 +0530

    Solution to Perform Bitwise One's Complement

---
+++ Perform Bitwise One's Complement@@67882a731b4392d8fc1577e7/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// char* welcome() {
+//     return "Welcome to Code Radar!";
+// }
+
+int main() {
+    // printf("%s", welcome());
+    int a;
+    scanf("%d",&a);
+    printf("%d",~a);
+    return 0;
+}
