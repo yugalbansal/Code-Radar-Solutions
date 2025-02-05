4113e9efa792de2746d882d5cacf080ae7da11fd
YUGAL BANSAL <BANSALYUGAL3@GMAIL.COM>
2025-02-05 09:40:06 +0530

    Solution to Determine Eligibility for Voting

--- Determine Eligibility for Voting@@6788dd63490637cfc44e1b99/code.c
+++ Determine Eligibility for Voting@@6788dd63490637cfc44e1b99/code.c
@@ -4,6 +4,6 @@ int main() {
     // printf("%s", welcome());
     int age;
     scanf("%d",&age);
-    (age>18)?printf("Eligible"):printf("Not Eligible");
+    (age>=18)?printf("Eligible"):printf("Not Eligible");
     return 0;
 }
