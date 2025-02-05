b48abd4cf81fa3e370f90bf711c25f4218d5e615
YUGAL BANSAL <BANSALYUGAL3@GMAIL.COM>
2025-02-05 09:59:52 +0530

    Solution to Check if a Number is Square of Another

--- Check if a Number is Square of Another@@6788dd63490637cfc44e1ba4/code.c
+++ Check if a Number is Square of Another@@6788dd63490637cfc44e1ba4/code.c
@@ -4,6 +4,6 @@ int main() {
     // printf("%s", welcome());
     int a,b;
     scanf("%d %d",&a,&b);
-    (b=a*a)?printf("Yes"):printf("No");
+    (b==a*a)?printf("Yes"):printf("No");
     return 0;
 }
