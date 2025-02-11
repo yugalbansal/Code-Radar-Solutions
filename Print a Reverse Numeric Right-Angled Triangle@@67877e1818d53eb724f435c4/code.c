4a3cb8cc293fe3df6971992577177115c0b3779e
YUGAL BANSAL <BANSALYUGAL3@GMAIL.COM>
2025-02-11 09:22:39 +0530

    Solution to Print a Reverse Numeric Right-Angled Triangle

---
+++ Print a Reverse Numeric Right-Angled Triangle@@67877e1818d53eb724f435c4/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // printf("%s", welcome());
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=n;j>=n;j--){
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
