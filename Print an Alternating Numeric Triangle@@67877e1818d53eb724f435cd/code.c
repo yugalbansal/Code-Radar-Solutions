5ad8a465f3ee2e03af6a219133c653a86edb8965
YUGAL BANSAL <BANSALYUGAL3@GMAIL.COM>
2025-03-21 10:26:08 +0530

    Solution to Print an Alternating Numeric Triangle

--- Print an Alternating Numeric Triangle@@67877e1818d53eb724f435cd/code.c
+++ Print an Alternating Numeric Triangle@@67877e1818d53eb724f435cd/code.c
@@ -1,14 +1,31 @@
-// Your code here...
+// // Your code here...
+// #include <stdio.h>
+// int main(){
+//     int a;
+//     scanf("%d",&a);
+//     for(int i=1;i<=a;i++){
+//         int n=0;
+//         for(int j=0;j<i;j++){
+//             if(i==1){printf("1");}
+//             else{printf("%d ",n+j);}
+//         }
+//         printf("\n");
+//     }
+// }
 #include <stdio.h>
-int main(){
-    int a;
-    scanf("%d",&a);
-    for(int i=1;i<=a;i++){
-        int n=0;
-        for(int j=0;j<i;j++){
-            if(i==1){printf("1");}
-            else{printf("%d ",n+j);}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        int num = i % 2; // Start with 0 if i is even, 1 if i is odd
+        for (int j = 0; j <= i; j++) {
+            printf("%d ", num);
+            num = 1 - num; // Toggle between 0 and 1
         }
         printf("\n");
     }
+
+    return 0;
 }
