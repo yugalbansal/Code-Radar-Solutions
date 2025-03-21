4e52285feb6b7d15aef91d8926c1d2e23f94a54c
YUGAL BANSAL <BANSALYUGAL3@GMAIL.COM>
2025-03-21 10:21:28 +0530

    Solution to Find the K-th Smallest Element

--- Find the K-th Smallest Element@@67ca7acd1fe08d7207c253c0/code.c
+++ Find the K-th Smallest Element@@67ca7acd1fe08d7207c253c0/code.c
@@ -1,19 +1,34 @@
-// Your code here...
-void selectionSort(int arr[],int n){
-    for(int i=0;i<n-1;i++){
+// // Your code here...
+// void selectionSort(int arr[],int n){
+//     for(int i=0;i<n-1;i++){
+//         int minIndex = i;
+//         for(int j=i+1;j<n;j++){
+//             if(arr[j],arr[minIndex]){
+//                 minIndex=j;
+//             }
+//         }
+//         int temp = arr[i];
+//         arr[i]=arr[minIndex];
+//         arr[minIndex]=temp;
+//     }
+// }
+
+// int KthSmallest(int arr[],int n,int k){
+//     selectionSort(arr,n);
+//     return arr[K-1];
+// }
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
         int minIndex = i;
-        for(int j=i+1;j<n;j++){
-            if(arr[j],arr[minIndex]){
-                minIndex=j;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
             }
         }
+        // Swap the found minimum element with the first element
         int temp = arr[i];
-        arr[i]=arr[minIndex];
-        arr[minIndex]=temp;
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
     }
-}
-
-int KthSmallest(int arr[],int n,int k){
-    selectionSort(arr,n);
-    return arr[K-1];
 }
