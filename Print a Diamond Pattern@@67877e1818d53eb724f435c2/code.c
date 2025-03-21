6d95806b71a0b7e505f804d572216c036318eb40
YUGAL BANSAL <BANSALYUGAL3@GMAIL.COM>
2025-03-21 10:31:18 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -1,36 +1,71 @@
+// #include <stdio.h>
+
+// // char* welcome() {
+// //     return "Welcome to Code Radar!";
+// // }
+
+// int main() {
+//     // printf("%s", welcome());
+//     int n;
+//     scanf("%d",&n);
+//     for(int i=1;i<=n;i++){
+//         for(int j=n;j>i;j--){
+//             printf(" ");
+//         }
+//         for(int j=1;j<=i;j++){
+//             printf("*");
+//         }
+//         for(int j=1;j<i;j++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             printf(" ");
+//         }
+//         for(int j=n;j>i;j--){
+//             printf("*");
+//         }
+//         for(int j=n-1;j>i;j--){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
 #include <stdio.h>
 
-// char* welcome() {
-//     return "Welcome to Code Radar!";
-// }
-
 int main() {
-    // printf("%s", welcome());
     int n;
-    scanf("%d",&n);
-    for(int i=1;i<=n;i++){
-        for(int j=n;j>i;j--){
+    scanf("%d", &n);
+
+    // Top half of the diamond
+    for (int i = 1; i <= n; i++) {
+        // Print leading spaces
+        for (int j = 1; j <= n - i; j++) {
             printf(" ");
         }
-        for(int j=1;j<=i;j++){
-            printf("*");
-        }
-        for(int j=1;j<i;j++){
+        // Print stars
+        for (int j = 1; j <= (2 * i - 1); j++) {
             printf("*");
         }
         printf("\n");
     }
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
+
+    // Bottom half of the diamond (N-1 rows)
+    for (int i = n - 1; i >= 1; i--) {
+        // Print leading spaces
+        for (int j = 1; j <= n - i; j++) {
             printf(" ");
         }
-        for(int j=n;j>i;j--){
-            printf("*");
-        }
-        for(int j=n-1;j>i;j--){
+        // Print stars
+        for (int j = 1; j <= (2 * i - 1); j++) {
             printf("*");
         }
         printf("\n");
     }
+
     return 0;
 }
+
