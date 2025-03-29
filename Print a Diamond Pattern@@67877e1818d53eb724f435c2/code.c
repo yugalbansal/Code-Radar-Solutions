12a5737a5a6c6967132cb4c48dd9a1c7b2cf1b75
YUGAL BANSAL <BANSALYUGAL3@GMAIL.COM>
2025-03-29 11:31:34 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -1,71 +1,27 @@
-// #include <stdio.h>
-
-// // char* welcome() {
-// //     return "Welcome to Code Radar!";
-// // }
-
-// int main() {
-//     // printf("%s", welcome());
-//     int n;
-//     scanf("%d",&n);
-//     for(int i=1;i<=n;i++){
-//         for(int j=n;j>i;j--){
-//             printf(" ");
-//         }
-//         for(int j=1;j<=i;j++){
-//             printf("*");
-//         }
-//         for(int j=1;j<i;j++){
-//             printf("*");
-//         }
-//         printf("\n");
-//     }
-//     for(int i=1;i<=n;i++){
-//         for(int j=1;j<=i;j++){
-//             printf(" ");
-//         }
-//         for(int j=n;j>i;j--){
-//             printf("*");
-//         }
-//         for(int j=n-1;j>i;j--){
-//             printf("*");
-//         }
-//         printf("\n");
-//     }
-//     return 0;
-// }
 #include <stdio.h>
 
-int main() {
-    int n;
-    scanf("%d", &n);
-
-    // Top half of the diamond
-    for (int i = 1; i <= n; i++) {
-        // Print leading spaces
-        for (int j = 1; j <= n - i; j++) {
+void pattern(int n){
+    for(int i=0;i<2*n-1;i++){
+        // space 
+        for(int j=1;j<n-i+1;j++){
             printf(" ");
         }
-        // Print stars
-        for (int j = 1; j <= (2 * i - 1); j++) {
+        // star
+        for(int j=0;j<=2*i+1;j++){
             printf("*");
+        }
+        // space
+        for(int j=1;j<n-i+1;j++){
+            printf(" ");
         }
         printf("\n");
     }
 
-    // Bottom half of the diamond (N-1 rows)
-    for (int i = n - 1; i >= 1; i--) {
-        // Print leading spaces
-        for (int j = 1; j <= n - i; j++) {
-            printf(" ");
-        }
-        // Print stars
-        for (int j = 1; j <= (2 * i - 1); j++) {
-            printf("*");
-        }
-        printf("\n");
-    }
-
-    return 0;
 }
 
+int main(){
+    int n;
+    scanf("%d",&n);
+    pattern(n);
+    return 0;
+}
