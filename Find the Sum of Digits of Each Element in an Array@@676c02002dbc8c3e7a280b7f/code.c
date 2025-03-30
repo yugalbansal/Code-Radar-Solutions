671bf8f2b5ffae11ba30f6bee7c1c034d0654dab
YUGAL BANSAL <BANSALYUGAL3@GMAIL.COM>
2025-03-30 11:10:25 +0530

    Solution to Find the Sum of Digits of Each Element in an Array

---
+++ Find the Sum of Digits of Each Element in an Array@@676c02002dbc8c3e7a280b7f/code.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int isPrime(int arr[],int n){
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+    }
+    return count;
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+   
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+        
+    }
+   int result=repeat(arr,n);
+   printf("%d",result);
+}
