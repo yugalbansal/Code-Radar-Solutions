debb0196b5cc5d056608514785426873135e3280
YUGAL BANSAL <BANSALYUGAL3@GMAIL.COM>
2025-03-30 11:41:07 +0530

    Solution to Check for Palindrome Array

--- Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
+++ Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
@@ -1,30 +1,28 @@
 #include <stdio.h>
 
-int ispallindrome(int n){
-    int a=n,digit,rev=0;
-    while(a>0){
-        digit = a%10;
-        rev=rev*10+digit;
-        a/=10;
-    }
-    return rev==n;
-}
-
 int main(){
     int n;
     scanf("%d",&n);
     int arr[n];
-    int freq[n];
-    for(int i=0;i<n;i++){
+      for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
-        freq[i]=-1;
     }
-    int number=0,dig;
-    for(int i=0;i<n;i++){
-        dig=arr[i];
-        number=number%10+dig;
-    } 
-    if(ispallindrome(number)){
+    int count=0;
+    if(n%2==0){
+        for(int i=1;i<=n/2;i++){
+            if(arr[i-1]==arr[n-i]){
+            count++;
+            }
+        }
+    }
+    else(
+        for(int i=1;i<(n-1)/2;i++){
+            if(arr[i-1]==arr[n-i]){
+                count++;
+            }
+        }
+    )
+    if(n/2==count){
         printf("YES");
     }
     else{
